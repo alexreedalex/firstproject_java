b23138fd618f6b80e7dffb0837f4a8a850cd21de
alexreedalex <106888287+alexreedalex@users.noreply.github.com>
2022-06-10 21:25:00 +0300

    first part

---
+++ src/lesson01/lesson.java
@@ -0,0 +1,36 @@
+package lesson01;
+
+public class lesson {
+    public static void main(String[] args) {
+        int myNumber = 5;
+//        System.out.print(myNumber);
+
+
+        byte myByte = -127;
+        byte myByte2 = 127;
+//        byte 0 -> 255
+
+        short myShort = -32000;
+        short myShort2 = 32000;
+//        0 -> 64000
+
+        int myInteger = -2_000_000_000;
+        int myInteger2 = 2_000_000_000;
+
+        long myLong = 9_200_000_000_000_000_000L;
+
+        float myFloat = 3.123456789123456f;
+        double myDouble = 3.123456789123456789;
+//        System.out.println(myFloat);
+//        System.out.println(myDouble);
+
+        boolean myBoolean = true;
+        boolean myBoolean2 = false;
+
+        char letter = 'a';
+        System.out.println(letter + 1);
+
+
+
+    }
+}
